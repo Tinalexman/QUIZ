9fcb185ee7cbc0c02a767f4be9d33949900ed045
Tinalexman <taiwoluwatobilobafestus@gmail.com>
2023-12-03 19:42:48 +0100

    Added the categories

--- src/components/home/Offers.jsx
+++ src/components/home/Offers.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 
-import Image from "next/image";
 import Arts from "@/public/Arts.png";
 import Food from "@/public/Food.png";
 import Geography from "@/public/Geography.png";
@@ -11,6 +10,7 @@ import Music from "@/public/Music.png";
 import Science from "@/public/Science.png";
 import Society from "@/public/Society.png";
 import Sports from "@/public/Sports.png";
+import OfferCard from "../reusable/OfferCard";
 
 const Offers = () => {
   const offers = [
@@ -65,21 +65,25 @@ const Offers = () => {
         Dive into a world of quizzes designed to entertain and educate
       </p>
 
-      <div className="sm:flex mt-20 sm:mt-10 sm:flex-col sm:w-full grid grid-cols-5 gap-5 w-full px-[10%]">
+      <div className="flex justify-around mt-20 sm:gap-10 sm:flex-col w-full px-[10%]">
         {offers.map((offer, i) => {
-          return (
-            <div
-              key={i}
-              className="flex flex-col bg-mainCyan w-[150px] h-[150px] rounded-lg shadow-xl sm:w-full items-center sm:px-[5%] justify-center"
-            >
-              <Image src={offer.image} className="w-[75px] h-[75px]" />
-              <p className="text-white mt-4 text-center text-[14px] font-medium cursor-default">
-                {offer.name}
-              </p>
-            </div>
+          return i < 5 ? (
+            <OfferCard key={i} image={offer.image} name={offer.name} />
+          ) : (
+            <></>
           );
         })}
       </div>
+      <div className="flex justify-around mt-20 sm:gap-10 sm:mt-10 sm:flex-col w-full px-[10%]">
+        {offers.map((offer, i) => {
+          return i >= 5 ? (
+            <OfferCard key={i} image={offer.image} name={offer.name} />
+          ) : (
+            <></>
+          );
+        })}
+      </div>
+      
     </div>
   );
 };
---
+++ src/components/reusable/OfferCard.jsx
@@ -0,0 +1,49 @@
+"use client";
+
+
+import React, {useState, useEffect} from "react";
+
+import Image from "next/image";
+import { motion } from "framer-motion";
+
+function randomStart() {
+  let x = Math.floor((Math.random() * 10) + 1);
+  return [0, x, -x, 0];
+  return x;
+}
+
+const OfferCard = ({ image, name, key }) => {
+  const [range, setRange] = useState([0, 0, 0, 0]);
+
+  useEffect(() => {
+    setRange(randomStart());
+  }, []);
+
+
+  return (
+    <motion.div
+      key={key}
+      animate={{
+        y: range,
+        transition: {
+          duration: 3,
+          repeat: Infinity,
+          ease: "easeInOut",
+          
+        },
+      }}
+
+      whileHover={{
+        scale: 1.2,
+      }}
+      className="flex flex-col bg-mainCyan w-[150px] h-[150px] rounded-lg shadow-xl sm:w-full items-center sm:px-[5%] justify-center"
+    >
+      <Image src={image} className="w-[75px] h-[75px]" alt=" "/>
+      <p className="text-white mt-4 text-center text-[14px] font-medium cursor-default">
+        {name}
+      </p>
+    </motion.div>
+  );
+};
+
+export default OfferCard;
